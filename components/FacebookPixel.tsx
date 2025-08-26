cf9963897add9560dbe02e06f227df80b999cf7d
Danny Sullivan <dsully15@gmail.com>
2025-08-26 12:34:45 -0400

    fixes pixel

--- components/FacebookPixel.tsx
+++ components/FacebookPixel.tsx
@@ -3,10 +3,12 @@
 import { useEffect } from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
+import Script from 'next/script'
 
 declare global {
   interface Window {
     fbq: any
+    _fbq: any
   }
 }
 
@@ -19,21 +21,6 @@ function FacebookPixelInner({ pixelId }: FacebookPixelProps) {
   const searchParams = useSearchParams()
 
   useEffect(() => {
-    if (!pixelId) return
-
-    // Initialize Facebook Pixel
-    import('react-facebook-pixel')
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init(pixelId)
-        ReactPixel.pageView()
-
-        // Set up global fbq function
-        window.fbq = ReactPixel.fbq
-      })
-  }, [pixelId])
-
-  useEffect(() => {
     if (!window.fbq) return
 
     // Track page views on route changes
@@ -44,10 +31,40 @@ function FacebookPixelInner({ pixelId }: FacebookPixelProps) {
 }
 
 export default function FacebookPixel({ pixelId }: FacebookPixelProps) {
+  if (!pixelId) return null
+
   return (
-    <Suspense fallback={null}>
-      <FacebookPixelInner pixelId={pixelId} />
-    </Suspense>
+    <>
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${pixelId}');
+            fbq('track', 'PageView');
+          `
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+        />
+      </noscript>
+      <Suspense fallback={null}>
+        <FacebookPixelInner pixelId={pixelId} />
+      </Suspense>
+    </>
   )
 }
 
